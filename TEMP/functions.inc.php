c6168dd473192a5a2f400abae419526d961f2140
Satyam Singh <satyam9322@gmail.com>
2021-11-27 15:05:06 +0530

    Made a dir. TEMP and replaced all 'users' with 'studs' in it

---
+++ TEMP/functions.inc.php
@@ -0,0 +1,141 @@
+<?php
+
+function emptyInputSignup($name,$email,$username,$pwd,$pwdRepeat){
+
+   $result = false;
+
+    if (empty($name)||empty($email)||empty($username)||empty($pwd)||empty($pwdRepeat)) {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidUid($username) {
+
+    $result = false;
+
+    if (!preg_match("/^[a-zA-z0-9]*$/",$username) ) {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidEmail($email) {
+
+    $result = false;
+
+    if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function pwdMatch($pwd,$pwdRepeat) {
+
+    $result = false;
+
+    if ($pwd !== $pwdRepeat) {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function uidExists($conn, $username,$email) {
+
+    $sql= "SELECT * FROM users  WHERE studsUid = ? OR studsEmail = ?;";
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        header("location: ../signup.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss",$username,$email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if($row = mysqli_fetch_assoc($resultData)){
+        return $row;
+    }
+    else{
+        $result = false;
+        return  $result;
+    }
+
+    mysqli_stmt_close($stmt);
+}
+
+function createUser($conn,$name,$email,$username,$pwd) {
+
+    $sql= "INSERT INTO users (studsName,studsEmail,studsUid,studsPwd) VALUES (?,?,?,?);";
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        header("location: ../signup.php?error=stmtfails");
+        exit();
+    }
+
+    $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "ssss",$name,$email,$username,$hashedPwd); 
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    header("location: ../login_page.html?error=none");
+    exit();
+}
+
+function emptyInputLogin($username,$pwd){
+
+    $result = false;
+ 
+     if (empty($username)||empty($pwd)) {
+         $result = true;
+     }
+     else{
+         $result = false;
+     }
+     return $result;
+ }
+
+ function loginUser($conn,$username,$pwd) {
+
+    $uidExists = uidExists($conn, $username,$username);
+
+    if ($uidExists == false) {
+        header("location: ../login.php?error=wronglogin");  
+    }
+
+    $pwdHashed = $uidExists["usersPwd"];
+    $checkPwd = password_verify($pwd,$pwdHashed);
+
+    if ($checkPwd == false) {
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+
+    else if ($checkPwd == true){
+        session_start();
+        $_SESSION['userid'] = $uidExists["studsId"];
+        $_SESSION['useruid'] = $uidExists["studsUid"];
+        header("location: ../dashboard.php");
+        exit();
+    }
+
+ }
